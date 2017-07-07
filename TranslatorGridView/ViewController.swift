b596dc83559f3fc56c94fba241f808caeb9c9a45
frind <info@frind.it>
2017-07-07 11:19:18 +0200

    Initial Commit

---
+++ TranslatorGridView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TranslatorGridView
+//
+//  Created by Luca Becchetti on 07/07/17.
+//  Copyright © 2017 Luca Becchetti. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
